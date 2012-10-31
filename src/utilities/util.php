c9e86a85793fee43e99a8bfa9a7d98029a43030c
ider <ider.cs@gmail.com>
2012-10-30 17:04:38 -0700

    Add more fuctionalities in Util decodefilename, foramt datatime with Y-m-d H:i:s

--- src/utilities/util.php
+++ src/utilities/util.php
@@ -10,17 +10,27 @@ class Util {
         if (LOG_ERROR) error_log($error);
     }
 
-
     public static function getErrors() {
         return self::$errors;
     }
 
+    const FILENAME_DELIMETER = '-';
+
     //trim white space and change space to underline
     public static function encodeFileName($name) {
-        return preg_replace('/[\t\r ]+/', '_', trim($name));
+        return preg_replace('/[\t\r _]+/', self::FILENAME_DELIMETER, trim($name));
+    }
+
+    public static function decodeFileName($name) {
+        return str_replace(self::FILENAME_DELIMETER, ' ', $name);
     }
 
     public static function hasTutorial($tutorialName) {
+        //TODO
+    }
 
+    const DATETIME_FORMAT = 'Y-m-d H:i:s';
+    public static function formatDatetime($datetime) {
+        return date(self::DATETIME_FORMAT, $datetime);
     }
 }
