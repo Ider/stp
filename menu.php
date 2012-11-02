75c4c8b680bd88065ab75f13eeb5d48ae147dcd6
ider <ider.cs@gmail.com>
2012-11-01 23:04:14 -0700

    add menu for switching page views
    style menu, hove it on the right side of windows

---
+++ menu.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" type="text/css" href="./css/menu.css" />
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+
+<div class="menu">
+    <ul>
+        <li><a href="tutorialview.php">Tutorials</a></li>
+        <li><a href="reviseview.php">Revise</a></li>
+        <li><a href="synchronizeview.php">Sync</a></li>
+    </ul>
+</div>
+
