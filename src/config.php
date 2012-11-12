95a4283801cf224ab36dec9e1d283da7ad904f14
ider <ider.cs@gmail.com>
2012-11-11 20:22:21 -0800

    add IS_PROD global constant
    using IS_PROD to set up config infomation

--- src/config.php
+++ src/config.php
@@ -1,14 +1,21 @@
 <?php
 
-define('DB_SERVER', '66.147.244.142');
+define('IS_PROD', false);
+
+if (IS_PROD) {
+    define('DB_SERVER', 'localhost');
+    define('DB_TABLE', 'stp');
+    define('CONTENT_DIR', dirname(__FILE__).'/../res/content/');
+} else {
+    define('DB_SERVER', '66.147.244.142');
+    define('DB_TABLE', 'stp_sample');
+    define('CONTENT_DIR', '/tmp/todo/');
+}
+
 define('DB_USER', 'iderzhen_project');
 define('DB_DATABSE', 'iderzhen_projects');
 define('DB_PASSWORD', 'oxyXN2x%R9PT');
-define('DB_TABLE', 'stp_sample');
 
-
-//define('CONTENT_DIR', dirname(__FILE__).'/../res/content/');
-define('CONTENT_DIR', '/tmp/todo/');
 
 define('LOG_ERROR', false);
 
